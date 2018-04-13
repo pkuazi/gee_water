f48d67721f813a858559a833941339d13982d0ee
zhenbing <zjh@cnic.cn>
2018-04-13 15:39:27 +0800

    water indices and image combine/cat

--- water.py
+++ water.py
@@ -7,8 +7,9 @@
 
 """
 qtp water extraction
-""" 
+"""
 import fiona
+
 # 第一步，读取标注的矢量文件，存储为earthengine的FeatureCollection类型
 shp_file = '/mnt/win/water_paper/training_data/TM/feature_shp/water-L5134036.shp'
 
@@ -22,8 +23,8 @@ for feature in vector:
     geojson = feature['geometry']
     geojson_list.append(geojson)
 
-
 import ee
+
 ee.Initialize()
 features = []
 for i in range(len(geojson_list)):
@@ -31,29 +32,50 @@ for i in range(len(geojson_list)):
     if type == 'Polygon':
         # Earth Engine's geometry constructors build geodesic geometries by default. To make a planar geometry, constructors have a geodesic parameter that can be set to false
         # 创建geometry时，默认是地理坐标；如果不是地理坐标，则geodesic参数设置为False
-        record = ee.Feature(ee.Geometry.Polygon(geojson_list[i]['coordinates'], proj=shp_proj,geodesic=False)).set('class','water')
+        record = ee.Feature(ee.Geometry.Polygon(geojson_list[i]['coordinates'], proj=shp_proj, geodesic=False)).set(
+            'class', 'water')
     elif type == 'MultiPolygon':
         # continue
-        record = ee.Feature(ee.Geometry.MultiPolygon(geojson_list[i]['coordinates'], proj=shp_proj,geodesic=False),{'class':'water'})
+        record = ee.Feature(ee.Geometry.MultiPolygon(geojson_list[i]['coordinates'], proj=shp_proj, geodesic=False),
+                            {'class': 'water'})
     elif type == 'Point':
-        record = ee.Feature(ee.Geometry.Point(geojson_list[i]['coordinates'], proj=shp_proj,geodesic=False), {'class':'water'})
+        record = ee.Feature(ee.Geometry.Point(geojson_list[i]['coordinates'], proj=shp_proj, geodesic=False),
+                            {'class': 'water'})
     features.append(record)
 
 # create a FeatureCollection from the list and print it.
 FCfromList = ee.FeatureCollection(features)
 
 # 第二步，读取标注的矢量文件对应的遥感影像,
-# method 1 通过dataid进行过滤,但可能因为数据版本原因找不到对应影像， 如 dataid= "LT51340362005235BJC00"，gee中LT51340362005235BJC01
-dataid='LT51340362005235BJC01'
-imagecol = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA').filterMetadata('LANDSAT_SCENE_ID', 'equals', dataid)
-image = ee.Image(imagecol.first())
+# method 1 通过dataid进行过滤,但可能因为数据版本原因找不到对应影像， 如 dataid= "LT51340362005235BJC00"，gee中LT51340362005235BJC01,所以Pass
+# dataid='LT51340362005235BJC01'
+# imagecol = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA').filterMetadata('LANDSAT_SCENE_ID', 'equals', dataid)
+# image = ee.Image(imagecol.first())
 
-# method 2 通过标注矢量文件范围，和时间
-roi = ee.Geometry.MultiPolygon(geojson_list[0]['coordinates'], proj=shp_proj,geodesic=False)
-image = ee.Image(ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA').filterBounds(roi).filterDate('2005-08-01','2005-08-30').sort('CLOUD_COVER').first())
-# other metadata properties: 'WRS_PATH': 134,  'WRS_ROW': 36,
+# method 2 通过标注矢量文件范围，和时间,但标注的影像可能有云，所以这个方法也不合适，Pass
+# roi = ee.Geometry.MultiPolygon(geojson_list[0]['coordinates'], proj=shp_proj,geodesic=False)
+# image = ee.Image(ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA').filterBounds(roi).filterDate('2005-08-01','2005-08-30').sort('CLOUD_COVER').first())
 
-# 第三步，计算各种water index
+# method 3 通过标注矢量文件范围(metadata properties: 'WRS_PATH': 134,  'WRS_ROW': 36)，和时间
+path = 134
+row = 36
+from datetime import datetime, timedelta
+
+feat_date = "2005-08-23"
+date = datetime.strptime(feat_date, "%Y-%m-%d")
+modified_date = date + timedelta(days=1)
+end_date = datetime.strftime(modified_date, "%Y-%m-%d")
+imageCol = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA').filterDate(feat_date, end_date).filterMetadata('WRS_PATH','equals',path).filterMetadata('WRS_ROW', 'equals', row).sort('CLOUD_COVER')
+image = ee.Image(imageCol.first())
+
+# read metadata and profile of the image for later image storage and download
+scene_id = image.get('LANDSAT_SCENE_ID').getInfo()
+bandNames = image.bandNames().getInfo()
+profile = image.projection().getInfo()
+crs = profile['crs']
+crs_transform = profile['transform']
+
+# 第三步，计算各种water index, 将指数特征波段加入到image中
 # example: evi = image.expression('2.5*((NIR - RED)/(NIR + 6*RED-7.5*BLUE+1))',{'NIR':image.select('B5'), 'RED':image.select('B4'), 'BLUE':image.select('B2')})
 # for landsat45: band1:blue, band2:green, band3:red, band4:nir, band5:swir5, band7:swir7
 # NDWI = (band4 - band5) / (band4 + band5)
@@ -63,13 +85,25 @@ image = ee.Image(ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA').filterBounds(roi)
 # NDWI_B = (band1 - band4) / (band1 + band4)
 # AWElnsh = 4 * (band2 - band5) - (0.25 * band4 + 2.75 * band7)
 
-NDWI = image.expression('(NIR - SWIR5)/(NIR + SWIR5)',{'NIR':image.select('B4'), 'SWIR5':image.select('B5')})
-MNDWI = image.expression('(GREEN - SWIR5)/(GREEN + SWIR5)',{'GREEN':image.select('B2'), 'SWIR5':image.select('B5')})
-EWI = image.expression('(GREEN - NIR - SWIR5)/(GREEN + NIR + SWIR5)',{'GREEN':image.select('B2'), 'NIR':image.select('B4'), 'SWIR5':image.select('B5')})
-NEW = image.expression('(BLUE - SWIR7)/(BLUE + SWIR7)',{'BLUE':image.select('B1'), 'SWIR7':image.select('B7')})
-NDWI_B = image.expression('(BLUE - NIR)/(BLUE+NIR)',{'BLUE':image.select('B1'), 'NIR':image.select('B4')})
-AWElnsh = image.expression('4*(GREEN-SWIR5)-(0.25*NIR+2.75*SWIR7)',{'GREEN':image.select('B2'),'SWIR5':image.select('B5'),'NIR':image.select('B4'),'SWIR7':image.select('B7')})
+NDWI = image.expression('(NIR - SWIR5)/(NIR + SWIR5)', {'NIR': image.select('B4'), 'SWIR5': image.select('B5')}).rename('NDWI')
+MNDWI = image.expression('(GREEN - SWIR5)/(GREEN + SWIR5)', {'GREEN': image.select('B2'), 'SWIR5': image.select('B5')}).rename('MNDWI')
+EWI = image.expression('(GREEN - NIR - SWIR5)/(GREEN + NIR + SWIR5)',{'GREEN': image.select('B2'), 'NIR': image.select('B4'), 'SWIR5': image.select('B5')}).rename('EWI')
+NEW = image.expression('(BLUE - SWIR7)/(BLUE + SWIR7)',{'BLUE': image.select('B1'), 'SWIR7': image.select('B7')}).rename('NEW')
+NDWI_B = image.expression('(BLUE - NIR)/(BLUE+NIR)', {'BLUE': image.select('B1'), 'NIR': image.select('B4')}).rename('NDWI_B')
+AWElnsh = image.expression('4*(GREEN-SWIR5)-(0.25*NIR+2.75*SWIR7)',{'GREEN': image.select('B2'), 'SWIR5': image.select('B5'), 'NIR': image.select('B4'),'SWIR7': image.select('B7')}).rename('AWElnsh')
+
 # To get a collection of random points in a specified region, you can use: Create 1000 random points in the region.
+# 将特征指数图片合并为一个image, 加入到image中
+feat_image = ee.Image.cat(NDWI, MNDWI, EWI, NEW, NDWI_B, AWElnsh)
+newimage = image.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7']).addBands(feat_image)
+bandNames = newimage.bandNames().getInfo()
+
+download_url = newimage.getDownloadURL(('water_indices.tif', bandNames, crs, crs_transform))
+# export results into raster file
+# TODO
+cmd = 'Export.image.toAsset({image: %s,  description: %s,  assetId: %s,   crs: %s,  crsTransform:%s})'%(newimage,'imageToAssetExample', 'exampleExport',crs,crs_transform)
+
+
 # randomPoints = ee.FeatureCollection.randomPoints(FCfromList);
 
 # 第四步， 标注矢量数据转化为训练样本数据
@@ -92,7 +126,7 @@ print('reading image data %s to be processed....' % imagepixel_trainingdata.imag
 # 将csv record格式的训练数据转换为ee的training数据格式
 whole_count, x_col = training_X.shape
 
-training_y=training_y.rename(columns = {0:'landcover'})
+training_y = training_y.rename(columns={0: 'landcover'})
 whole_dt = pd.merge(training_X, training_y, left_index=True, right_index=True)
 
 print(whole_dt.iloc[0])
@@ -104,13 +138,13 @@ for i in range(whole_count):
     trainFeat.append(trainpixel)
 trainFC = ee.FeatureCollection(trainFeat)
 
-# 第五步 训练模型，并将指数特征波段加入到image中
+# 第五步 训练模型，并
+
 # feature attributes list
 train_bands = list(whole_dt.columns.values)
 trained = ee.Classifier.cart().train(trainFC, 'landcover', train_bands)
 
 # classify the image with the same bands used for training
-newimage = image.addBands(NDWI, MNDWI, EWI, NEW, NDWI_B, AWElnsh)
 classified = newimage.select(train_bands).classify(trained)
 
 # 最后一步，结果导出
@@ -655,4 +689,3 @@ landcover:
 # # // Print the confusion matrix.
 # confusionMatrix = test.errorMatrix(classProperty, 'classification');
 # print('Confusion Matrix', confusionMatrix);
-
